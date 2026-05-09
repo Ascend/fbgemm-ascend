a96a304ad714cbe331822b02d3472f458c4c1b4f
Zhz_Bey <1209023967@qq.com>
2026-05-09 17:46:29 +0800

    feat: add linearize_cache_indices_from_row_idx op for Ascend A5 part1
    Co-authored-by: Zhz_Bey<1209023967@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !80 merge feat/linearize-cache-indices-from-row-idx into v1.2.0
    
    feat: add linearize_cache_indices_from_row_idx op for Ascend A5 part1
    
    Created-by: Zhz_Bey
    Commit-by: Zhz_Bey
    Merged-by: ascend-robot
    Description: ## 合入来源
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue
    
    ## 问题/功能描述
    
    ## 修改方案描述
    
    ## 开发自检
    
    - [x] 安全：涉及对外接口/参数/环境变量改动，并同步修改资料
    - [x] 安全：是否引入三方开源软件，并经过安全评审
    - [x] 安全：是否新增对外接口/参数，并进行校验
    - [x] 规范：是否涉及资料改动，并已完成修改
    - [x] 规范：新增功能随DT上库
    
    See merge request: Ascend/fbgemm-ascend!80

--- FbgemmAscend.cmake
+++ FbgemmAscend.cmake
@@ -13,6 +13,7 @@ set(ASCENDC_A5_ONLY_OPS
     pruned_hashmap_lookup
     group_index_select_dim0
     group_index_select_dim0_backward
+    linearize_cache_indices_from_row_idx
 )
 
 set(ASCENDC_A3_OPS
@@ -103,6 +104,7 @@ set(_ASCENDC_OPS
     "expand_into_jagged_permute|${FBGEMM_ASCEND_SOURCE_DIR}/src/sparse_ops/expand_into_jagged_permute"
     "invert_permute|${FBGEMM_ASCEND_SOURCE_DIR}/src/sparse_ops/invert_permute"
     "linearize_cache_indices|${FBGEMM_ASCEND_SOURCE_DIR}/src/split_embeddings_cache/linearize_cache_indices"
+    "linearize_cache_indices_from_row_idx|${FBGEMM_ASCEND_SOURCE_DIR}/src/split_embeddings_cache/linearize_cache_indices_from_row_idx"
     "offsets_range|${FBGEMM_ASCEND_SOURCE_DIR}/src/sparse_ops/offsets_range"
     "permute2d_sparse_data|${FBGEMM_ASCEND_SOURCE_DIR}/src/sparse_ops/permute2d_sparse_data"
     "segment_sum_csr|${FBGEMM_ASCEND_SOURCE_DIR}/src/sparse_ops/segment_sum_csr"
@@ -175,6 +177,7 @@ set(FBGEMM_ASCEND_ADAPTER_SRCS
     src/sparse_ops/expand_into_jagged_permute/expand_into_jagged_permute.cpp
     src/sparse_ops/invert_permute/invert_permute.cpp
     src/split_embeddings_cache/linearize_cache_indices/linearize_cache_indices.cpp
+    src/split_embeddings_cache/linearize_cache_indices_from_row_idx/linearize_cache_indices_from_row_idx.cpp
     src/sparse_ops/offsets_range/offsets_range.cpp
     src/sparse_ops/permute2d_sparse_data/permute1d_sparse_data.cpp
     src/sparse_ops/permute2d_sparse_data/permute2d_sparse_data.cpp
