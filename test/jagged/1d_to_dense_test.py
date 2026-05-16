c4ed0ca234c609f7e03f1eda5c7deb52e3be8dc6
alliswell1024 <liangrenhao1@huawei.com>
2026-05-16 10:30:55 +0800

    [FEAT]add jagged_1d_to_dense test
    Co-authored-by: liangrenhao<liangrenhao1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !151 merge v1.5.0_02 into v1.5.0
    
    [FEAT]add jagged_1d_to_dense test
    
    Created-by: alliswell1024
    Commit-by: liangrenhao
    Merged-by: ascend-robot
    Description: ## 合入来源
    
    - [x] 需求
    - [ ] 问题单
    - [ ] issue
    
    ## 是否涉及test文件修改
    
    - [ ] 是
    - [ ] 否（说明理由）
    
    ## 问题/功能描述
    
    ## 修改方案描述
    
    ## 开发自检
    
    - [x] 安全：涉及对外接口/参数/环境变量改动，并同步修改资料
    - [x] 安全：是否引入三方开源软件，并经过安全评审
    - [x] 安全：是否新增对外接口/参数，并进行校验
    - [x] 规范：是否涉及资料改动，并已完成修改
    - [x] 规范：新增功能随DT上库
    
    See merge request: Ascend/fbgemm-ascend!151

---
+++ test/jagged/1d_to_dense_test.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import unittest
+
+import fbgemm_ascend  # noqa: F401
+import hypothesis.strategies as st
+import numpy as np
+import torch
+from hypothesis import Verbosity, given, settings
+
+
+# Converts lengths + values format to COO format
+# [B], [N] -> [B, N'].
+# pyre-ignore Missing return annotation [3]
+def var_list_to_coo_1d(
+    lengths: torch.Tensor,
+    values: torch.Tensor,
+    N: int,
+):
+    # pyre-ignore[21]
+    def lengths_to_segment_ids(lengths: torch.Tensor) -> torch.Tensor:
+        return torch.repeat_interleave(
+            torch._dim_arange(lengths, 0).long(),
+            lengths.long(),
+        )
+
+    rows = lengths_to_segment_ids(lengths)
+    num_rows = lengths.size()[0]
+    # This does D&H sync
+    offsets = torch.ops.fbgemm.asynchronous_complete_cumsum(lengths)
+    output_size = lengths.sum()
+    # This does D&H sync
+    cols = torch.ops.fbgemm.offsets_range(offsets, output_size)
+    indices = torch.stack([rows, cols])
+    dims = [num_rows, N]
+    # torch.sparse_coo_tensor is not supported by torch.fx, wrap it.
+    return torch.sparse_coo_tensor(
+        indices=indices,
+        values=values,
+        size=dims,
+    )
+
+
+class Jagged1DToDenseTest(unittest.TestCase):
+    @settings(
+        verbosity=Verbosity.verbose,
+        max_examples=20,
+        deadline=None,
+    )
+    @given(
+        B=st.integers(min_value=1, max_value=128),
+        max_sequence_length=st.integers(min_value=1, max_value=500),
+        padding_value=st.integers(min_value=-100000, max_value=100000),
+    )
+    def test_jagged_1d_to_dense(
+        self,
+        B: int,
+        max_sequence_length: int,
+        padding_value: int,
+    ) -> None:
+        lengths_ = np.random.randint(low=0, high=max_sequence_length, size=B)
+        total_lengths = lengths_.sum()
+        lengths = torch.from_numpy(lengths_)
+        offsets = torch.ops.fbgemm.asynchronous_complete_cumsum(lengths)
+
+        ref_values = torch.randint(low=0, high=1000000000, size=(total_lengths,))
+        ref_values_mask = var_list_to_coo_1d(lengths, torch.ones_like(ref_values), max_sequence_length).to_dense()
+        ref_output_values = (
+            var_list_to_coo_1d(
+                lengths,
+                ref_values,
+                max_sequence_length,
+            ).to_dense()
+            + (1 - ref_values_mask) * torch.ones_like(ref_values_mask) * padding_value
+        )
+
+        # test cpu forward
+        values = ref_values.clone().detach().requires_grad_(False)
+        output_values = torch.ops.fbgemm.jagged_1d_to_dense(
+            values=values,
+            offsets=offsets,
+            max_sequence_length=max_sequence_length,
+            padding_value=padding_value,
+        )
+        torch.testing.assert_close(ref_output_values, output_values)
+
+        if torch.npu.is_available():
+            # test npu forward
+            ref_values = ref_values.npu()
+            values = ref_values.clone().detach().requires_grad_(False)
+            offsets = offsets.npu()
+            ref_output_values = ref_output_values.npu()
+            output_values = torch.ops.fbgemm.jagged_1d_to_dense(
+                values=values,
+                offsets=offsets,
+                max_sequence_length=max_sequence_length,
+                padding_value=padding_value,
+            )
+            torch.testing.assert_close(ref_output_values, output_values)
+
+    def test_jagged_1d_to_dense_truncation(self) -> None:
+        lengths_ = np.array([1, 3, 0, 1])
+        lengths = torch.from_numpy(lengths_)
+        offsets = torch.ops.fbgemm.asynchronous_complete_cumsum(lengths)
+
+        ref_values = torch.from_numpy(np.array([100, 3, 4, 5, 6]))
+        ref_output = torch.from_numpy(np.array([100, 3, -1, 6])).reshape(-1, 1)
+
+        # test cpu forward
+        values = ref_values.clone().detach().requires_grad_(False)
+        output = torch.ops.fbgemm.jagged_1d_to_dense(
+            values=values,
+            offsets=offsets,
+            max_sequence_length=1,
+            padding_value=-1,
+        )
+        torch.testing.assert_close(ref_output, output)
+
+        if torch.npu.is_available():
+            # test npu forward
+            ref_values = ref_values.npu()
+            values = ref_values.clone().detach().requires_grad_(False)
+            offsets = offsets.npu()
+            ref_output = ref_output.npu()
+            output = torch.ops.fbgemm.jagged_1d_to_dense(
+                values=values,
+                offsets=offsets,
+                max_sequence_length=1,
+                padding_value=-1,
+            )
+            torch.testing.assert_close(ref_output, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
